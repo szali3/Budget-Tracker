f8dbd7ab0df55af3031e2fb661b5e62adf723067
Zohaib Ali <62861582+szali3@users.noreply.github.com>
2021-08-25 11:37:41 -0400

    added caching service

--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -1,1 +1,58 @@
 
+const APP_PREFIX = "BudgetTracker-";
+const VERSION = "version_01";
+const CACHE_NAME = APP_PREFIX + VERSION;
+
+// files to be cached
+const FILES_TO_CACHE = [
+	"/index.html",
+	"/js/idb.js",
+	"/js/index.js",
+	"/css/styles.css",
+];
+
+// listening to itself for fetch then call function that checks if cache already present 
+self.addEventListener("fetch", function (e) {
+	console.log("fetch request : " + e.request.url);
+	e.respondWith(
+		caches.match(e.request).then(function (request) {
+			if (request) { // if request true return cache and not fetch
+				console.log("responding with cache : " + e.request.url);
+				return request;
+			} else { // if request false then return fetch as there is no cache
+				console.log("file is not cached, fetching : " + e.request.url);
+				return fetch(e.request);
+			}
+		})
+	);
+});
+
+// listening to itself for install then cachses all the hml and js files listed
+self.addEventListener("install", function (e) {
+	e.waitUntil(
+		caches.open(CACHE_NAME).then(function (cache) {
+			console.log("installing cache : " + CACHE_NAME);
+			return cache.addAll(FILES_TO_CACHE);
+		})
+	);
+});
+
+// delete outdated cahce
+self.addEventListener("activate", function (e) {
+	e.waitUntil(
+		caches.keys().then(function (keyList) {
+			let cacheKeeplist = keyList.filter(function (key) {
+				return key.indexOf(APP_PREFIX);
+			});
+			cacheKeeplist.push(CACHE_NAME);
+			return Promise.all(
+				keyList.map(function (key, i) {
+					if (cacheKeeplist.indexOf(key) === -1) {
+						console.log("deleting cache : " + keyList[i]);
+						return caches.delete(keyList[i]);
+					}
+				})
+			);
+		})
+	);
+});
