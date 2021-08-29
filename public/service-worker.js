c9fcde3ae204234948e294d7a7887cc99c94dc38
Zohaib Ali <s.zohaibali3@gmail.com>
2021-08-29 13:05:21 -0400

    fixed cache file name

--- public/service-worker.js
+++ public/service-worker.js
@@ -9,15 +9,7 @@ const FILES_TO_CACHE = [
   './js/idb.js',
   './js/index.js',
   './manifest.json',
-  './css/style.css',
-  '/icons/icon-72x72.png',
-  '/icons/icon-96x96.png',
-  '/icons/icon-128x128.png',
-  '/icons/icon-144x144.png',
-  '/icons/icon-152x152.png',
-  '/icons/icon-192x192.png',
-  '/icons/icon-384x384.png',
-  '/icons/icon-512x512.png'
+  './css/styles.css'
 ];
 
 // listening to itself for install then cachses all the hml and js files listed
